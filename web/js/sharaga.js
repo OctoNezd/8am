c747414672bf42891b03e45e184dc6b6e122e04d
Yan <me@octonezd.me>
2022-09-03 16:04:01 +0300

    Enable pwa support

--- web/js/sharaga.js
+++ web/js/sharaga.js
@@ -4,6 +4,18 @@ import "tom-select/dist/css/tom-select.css";
 import indexhtml from "index.html";
 import "/css/index.css";
 document.body.innerHTML = indexhtml;
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/service-worker.js")
+            .then((registration) => {
+                console.log("SW registered: ", registration);
+            })
+            .catch((registrationError) => {
+                console.log("SW registration failed: ", registrationError);
+            });
+    });
+}
 const icalpreview = document.getElementById("icalpreview");
 const ua = navigator.userAgent.toLowerCase();
 const isAndroid = ua.indexOf("android") > -1;
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+var WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 module.exports = {
     entry: "./js/sharaga.js",
@@ -24,6 +26,28 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "template.html",
             title: "Ш А Р А Г А",
+            favicon: "./icons/favicon.png",
+        }),
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true,
+        }),
+        new WebpackPwaManifest({
+            publicPath: "/",
+            name: "Ш А Р А Г А",
+            short_name: "Ш А Р А Г А",
+            description:
+                "Генератор ICS файлов и альтернативный интерфейс расписания",
+            background_color: "#ffffff",
+            crossorigin: "use-credentials", //can be null, use-credentials or anonymous
+            icons: [
+                {
+                    src: path.resolve("icons/favicon.png"),
+                    sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+                },
+            ],
         }),
     ],
     resolve: {
