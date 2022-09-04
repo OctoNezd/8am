a6b784703ead98bbaf5d427d6777a4b5b0745e66
Yan <me@octonezd.me>
2022-09-04 21:36:27 +0300

    Change theme color according to device theme and modal

--- web/js/pwa.js
+++ web/js/pwa.js
@@ -1,4 +1,5 @@
 let deferredPrompt;
+const metaThemeColor = document.querySelector('meta[name="theme-color"]');
 const ua = navigator.userAgent.toLowerCase();
 const isAndroid = ua.indexOf("android") > -1;
 const isIphone =
@@ -80,10 +81,12 @@ function setup_pwa_modal() {
     button.addEventListener("click", () => {
         modal.classList.add("open");
         document.body.classList.add("modal-open");
+        updateThemeColor();
     });
     const closeModal = function () {
         modal.classList.remove("open");
         document.body.classList.remove("modal-open");
+        updateThemeColor();
     };
     modal.addEventListener("click", closeModal);
     const controls = document.querySelector("#controls");
@@ -109,3 +112,59 @@ function setup_pwa_modal() {
     modalBody.appendChild(settingsApplyButton);
     document.body.appendChild(modal);
 }
+function hexToRGB(hex) {
+    var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+    return [r, g, b, 1];
+}
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+function getBgColor() {
+    let bgcolor;
+    if (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+        bgcolor = "#303030";
+    } else {
+        bgcolor = "#ffffff";
+    }
+    if (document.querySelector(".modal.open") !== null) {
+        var base = hexToRGB(bgcolor);
+        var added = [0, 0, 0, 0.3];
+
+        var mix = [];
+        mix[3] = 1 - (1 - added[3]) * (1 - base[3]); // alpha
+        mix[0] = Math.round(
+            (added[0] * added[3]) / mix[3] +
+                (base[0] * base[3] * (1 - added[3])) / mix[3]
+        ); // red
+        mix[1] = Math.round(
+            (added[1] * added[3]) / mix[3] +
+                (base[1] * base[3] * (1 - added[3])) / mix[3]
+        ); // green
+        mix[2] = Math.round(
+            (added[2] * added[3]) / mix[3] +
+                (base[2] * base[3] * (1 - added[3])) / mix[3]
+        ); // blue
+        mix = mix.slice(undefined, 3);
+        console.log("new rgb", mix);
+        bgcolor = rgbToHex(...mix);
+    }
+    return bgcolor;
+}
+
+function updateThemeColor() {
+    metaThemeColor.setAttribute("content", getBgColor());
+}
+
+window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", updateThemeColor);
+updateThemeColor();
