614a9f86d8aca008edbf0b8fbbc3eb0a39c6585a
Yan <me@octonezd.me>
2022-09-05 16:33:07 +0300

    Remove image processing

--- web/webpack.prod.js
+++ web/webpack.prod.js
@@ -1,7 +1,5 @@
 const { merge } = require("webpack-merge");
 const TerserPlugin = require("terser-webpack-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
-const { extendDefaultPlugins } = require("svgo");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
@@ -15,42 +13,6 @@ module.exports = merge(common, {
                 terserOptions: {
                     format: {
                         comments: false,
-                    },
-                },
-            }),
-            new ImageMinimizerPlugin({
-                minimizer: {
-                    implementation: ImageMinimizerPlugin.imageminMinify,
-                    options: {
-                        // Lossless optimization with custom option
-                        // Feel free to experiment with options for better result for you
-                        plugins: [
-                            ["gifsicle", { interlaced: true }],
-                            ["jpegtran", { progressive: true }],
-                            ["optipng", { optimizationLevel: 5 }],
-                            // Svgo configuration here https://github.com/svg/svgo#configuration
-                            [
-                                "svgo",
-                                {
-                                    plugins: extendDefaultPlugins([
-                                        {
-                                            name: "removeViewBox",
-                                            active: false,
-                                        },
-                                        {
-                                            name: "addAttributesToSVGElement",
-                                            params: {
-                                                attributes: [
-                                                    {
-                                                        xmlns: "http://www.w3.org/2000/svg",
-                                                    },
-                                                ],
-                                            },
-                                        },
-                                    ]),
-                                },
-                            ],
-                        ],
                     },
                 },
             }),
