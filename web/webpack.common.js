37634f0874f7b0cbf01e1ce76400d1d58a52cd2c
Yan <me@octonezd.me>
2022-09-06 16:51:58 +0300

    Include screenshots in PWA install

--- web/webpack.common.js
+++ web/webpack.common.js
@@ -4,6 +4,7 @@ const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 const webpack = require("webpack");
 const path = require("path");
 const WebpackBar = require("webpackbar");
+const CopyPlugin = require("copy-webpack-plugin");
 
 let commitHash = require("child_process")
     .execSync("git rev-parse --short HEAD")
@@ -39,17 +40,48 @@ module.exports = {
             logo: "./icons/main_icon.png",
             devMode: "webapp",
             mode: "webapp",
+            manifest: {
+                screenshots: [
+                    {
+                        src: "screenshot1.png",
+                        sizes: "1080x2400",
+                        type: "image/png",
+                        platform: "narrow",
+                        label: "Домашняя страница",
+                    },
+                    {
+                        src: "screenshot2.png",
+                        sizes: "1080x2400",
+                        type: "image/png",
+                        platform: "narrow",
+                        label: "Настройки",
+                    },
+                    {
+                        src: "pc.jpg",
+                        sizes: "480x360",
+                        type: "image/jpeg",
+                        platform: "narrow",
+                        label: "2pacaliveinserbia",
+                    },
+                ],
+            },
             favicons: {
                 orientation: "portrait",
                 maskable: true,
                 appName: "Ш А Р А Г А",
-                appDescription: "Ш А Р А Г А",
+                appDescription:
+                    "Веб-Приложение для просмотра расписания шараги",
                 developerURL: null, // prevent retrieving from the nearest package.json
                 background: "#ffffff",
                 theme_color: "#303030",
                 manifestMaskable: "./icons/maskable.png",
                 icons: {
                     favicons: false,
+                },
+                files: {
+                    android: {
+                        manifestFileName: "manifest.json",
+                    },
                 },
             },
         }),
@@ -67,6 +99,9 @@ module.exports = {
                     yandex: false,
                 },
             },
+        }),
+        new CopyPlugin({
+            patterns: [{ from: "assets", to: "assets" }],
         }),
         new WorkboxPlugin.GenerateSW({
             // these options encourage the ServiceWorkers to get in there fast
