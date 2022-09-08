23a475b9932e03048ee8e5617d0c89ad2b174c7b
Yan <me@octonezd.me>
2022-09-08 20:36:55 +0300

    Fix modal discard by accident

--- web/js/modal.js
+++ web/js/modal.js
@@ -4,7 +4,12 @@ export function openModal(modal) {
     modal.classList.add("open");
     document.body.classList.add("modal-open");
 }
-window.discardModal = function () {
+window.discardModal = function (e) {
+    console.log("discardModal", e, e.target);
+    if (e !== undefined && !e.target.classList.contains("modal")) {
+        console.log("ignoring discardmodal cause inside of modal");
+        return;
+    }
     document.body.classList.remove("modal-open");
     document
         .querySelectorAll(".modal.open")
