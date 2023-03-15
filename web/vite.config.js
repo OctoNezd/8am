f6689a7f65315ab507dab95dc21709a008633a83
octo <me@octonezd.me>
2023-03-15 23:44:30 +0300

    Revert "split into vendor chunks"
    This reverts commit ebfd87712a7615f3a457dc57b0b5d9e1be600c45.

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,13 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 import { gitDescribeSync } from 'git-describe'
-import { splitVendorChunkPlugin } from 'vite'
 const devapi = 'http://127.0.0.1:8000'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
-        splitVendorChunkPlugin(),
         VitePWA({
             mode: 'development',
             strategies: 'injectManifest',
